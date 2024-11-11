9d215f9d0c771f73c6f04c8c581f53dfcc8aab86
bezst <bezst29@gmail.com>
2024-11-11 17:53:46 +0200

    add DataModelContext

---
+++ ApiDomain/DataModelContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace ApiDomain
+{
+    public class DataModelContext : DbContext
+    {
+        public DataModelContext(DbContextOptions<DataModelContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
