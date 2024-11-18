76d3508a0d8fc6f7c0aa4a5f7d7082404b1940b4
bezst <bezst29@gmail.com>
2024-11-18 11:21:06 +0200

    Add WatchedMoviesCount to UserOutputDto

--- WebApp/Dtos/User/UserProfile.cs
+++ WebApp/Dtos/User/UserProfile.cs
@@ -9,7 +9,8 @@ namespace WebApp.Dtos.User
        {
             CreateMap<UserCreateDto, UserEntity>()
                 .ForMember(dest => dest.WatchedMovies, opt => opt.Ignore());
-            CreateMap<UserEntity, UserOutputDto>();
+            CreateMap<UserEntity, UserOutputDto>()
+                .ForMember(dest => dest.WatchedMoviesCount, opt => opt.MapFrom(src => src.WatchedMovies.Count));
         }
     }
 }
