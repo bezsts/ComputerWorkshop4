167ea4c5019446c16744f73359b26a2669c7db32
bezst <bezst29@gmail.com>
2024-11-19 15:01:41 +0200

    Add validation

---
+++ WebApp/Validators/MovieValidator.cs
@@ -0,0 +1,37 @@
+﻿using ApiDomain.Enums;
+using FluentValidation;
+using WebApp.Dtos.Movie;
+
+namespace ApiDomain.Validators
+{
+    public class MovieValidator : AbstractValidator<MovieCreateDto>
+    {
+        public MovieValidator()
+        {
+            RuleFor(m => m.Title)
+                .NotEmpty()
+                .MaximumLength(200);
+            
+            RuleFor(m => m.Director)
+                .NotEmpty()
+                .MaximumLength(200)
+                .Matches(@"^[a-zA-Z\s]+$").WithMessage("Director's name can only contain letters");
+            
+            RuleFor(m => m.Genre)
+                .IsInEnum()
+                .WithMessage($"Invalid genre specified. " +
+                $"Valid options are: {string.Join(", ", Enum.GetNames(typeof(Genre)))}");
+
+            RuleFor(m => m.ReleaseDate)
+                .NotEmpty();
+
+            When(m => m.IsReleased, () =>
+            {
+                RuleFor(m => m.ReleaseDate).LessThanOrEqualTo(DateOnly.FromDateTime(DateTime.Now));
+            }).Otherwise(() =>
+            {
+                RuleFor(m => m.ReleaseDate).GreaterThan(DateOnly.FromDateTime(DateTime.Now));
+            });
+        }
+    }
+}
---
+++ WebApp/Validators/UserValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using WebApp.Dtos.User;
+
+namespace ApiDomain.Validators
+{
+    public class UserValidator : AbstractValidator<UserCreateDto>
+    {
+        public UserValidator()
+        {
+            RuleFor(u => u.Name)
+                .NotEmpty()
+                .Length(4, 200);
+            
+            RuleFor(u => u.Email)
+                .NotEmpty()
+                .EmailAddress();
+        }
+    }
+}
