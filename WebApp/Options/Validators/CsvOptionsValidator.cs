aebf2ce88579a1c9fc8a8fda8739ca927f8f1c60
bezst <bezst29@gmail.com>
2024-12-06 14:59:31 +0200

    Add validation to configuration

---
+++ WebApp/Options/Validators/CsvOptionsValidator.cs
@@ -0,0 +1,39 @@
+﻿using FluentValidation;
+
+namespace WebApp.Options.Validators
+{
+    public class CsvOptionsValidator : AbstractValidator<CsvOptions>
+    {
+        public CsvOptionsValidator()
+        {
+            RuleFor(o => o.Delimiter)
+                .Must(d => Char.IsSeparator(d))
+                .NotEmpty();
+
+            RuleFor(o => o.DateFormat)
+                .Must(IsDateFormat)
+                .NotEmpty();
+
+            RuleFor(o => o.MaxExportRecords)
+                .GreaterThan(0)
+                .NotEmpty();
+
+            RuleFor(o => o.FieldsToExport)
+                .NotEmpty()
+                .ForEach(field => field.NotEmpty());
+        }
+
+        private bool IsDateFormat(string dateFormat)
+        {
+            try
+            {
+                DateOnly.FromDateTime(DateTime.Now).ToString(dateFormat);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ WebApp/Options/Validators/ExportMovieOptionsValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace WebApp.Options.Validators
+{
+    public class ExportMovieOptionsValidator : AbstractValidator<ExportMoviesOptions>
+    {
+        public ExportMovieOptionsValidator()
+        {
+            RuleFor(o => o.FileName)
+                .NotEmpty()
+                .Matches(@"^[a-zA-Z0-9_\-\.]+$")
+                .WithMessage("FileName can only contain letters, numbers, underscores, dashes, and dots.");
+        }
+    }
+}
